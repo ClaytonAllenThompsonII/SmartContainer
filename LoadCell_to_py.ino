433d066540940487c50f06e2573f6dc682d88403
CTII <clay.thompson13@gmail.com>
2023-03-19 17:34:13 -0500

    🧪 Initial commit for LoadCell_to_py
    - basic Arduino code for Real_Time Plot. 
    - need to add HX711 library.

---
+++ LoadCell_to_py.ino
@@ -0,0 +1,22 @@
+// Define the calibration factor for your load sensor
+const float CALIBRATION_FACTOR = 219.9;
+
+// Create an instance of the HX711 library
+HX711 loadSensor;
+
+void setup() {
+  Serial.begin(9600);
+  loadSensor.begin(2, 3); // Set the data and clock pins for the load sensor
+  loadSensor.set_scale(CALIBRATION_FACTOR);
+  loadSensor.tare();
+}
+
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == 'x') {
+      float load = loadSensor.get_units();
+      Serial.println(load);
+    }
+  }
+}
