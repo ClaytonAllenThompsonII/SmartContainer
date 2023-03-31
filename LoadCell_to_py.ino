9c60ec12b756cd45cdd4f387cc3957e4f0eeed5a
CTII <clay.thompson13@gmail.com>
2023-03-31 01:55:12 -0500

    🪩 Add updated Calibration Factor to LoacCell_to_py.ino
    - negative 1760

--- LoadCell_to_py.ino
+++ LoadCell_to_py.ino
@@ -1,13 +1,13 @@
 #include "HX711.h" //Original Repository Created by Bodge https://github.com/bogde/HX711
 // Define the calibration factor for your load sensor
-const float CALIBRATION_FACTOR = 219.9;
+const float CALIBRATION_FACTOR = -1760;
 
 // Create an instance of the HX711 library
 HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  scale.begin(2, 3); // Set the data and clock pins for the load sensor
+  scale.begin(3, 2); // Set the data and clock pins for the load sensor
   scale.set_scale(CALIBRATION_FACTOR);
   scale.tare();
 }
