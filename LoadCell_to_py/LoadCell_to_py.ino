daa676948dee8e55040aa7215fcc0d66486d39c5
CTII <clay.thompson13@gmail.com>
2023-04-30 02:14:48 -0500

    🪩 Add pinMode(), Set_offset() to LoadCell_to-py.ino
    - Also switched the data and ClocK pins. 
    - Begin() takes data, clock.

--- LoadCell_to_py/LoadCell_to_py.ino
+++ LoadCell_to_py/LoadCell_to_py.ino
@@ -2,14 +2,25 @@
 // Define the calibration factor for your load sensor
 const float CALIBRATION_FACTOR = -1760;
 
+long setting_tare_point; //Zero value that is found when scale is tared
+const byte statusLED = 13;  //Flashes with each reading
 // Create an instance of the HX711 library
 HX711 scale;
 
-void setup() {
+void setup() 
+{
+  pinMode(statusLED, OUTPUT);
+  
+  scale.begin(2, 3); // Set the data and clock pins for the load sensor
+
+  //Setup UART
   Serial.begin(9600);
-  scale.begin(3, 2); // Set the data and clock pins for the load sensor
+
   scale.set_scale(CALIBRATION_FACTOR);
+  scale.set_offset(setting_tare_point);
   scale.tare();
+
+
 }
 
 void loop() {
